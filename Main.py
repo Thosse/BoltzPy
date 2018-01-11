412dfa3a97d9e325b11168303bde3d0496f9ca31
Thosa <thomas.sasse@tu-ilmenau.de>
2018-01-11 13:03:43 +0100

    Added main.py In the long run this will be the High level script to run a simulation. Until then it will be used as a Road Map, what functionalities need to be implenented

---
+++ Main.py
@@ -0,0 +1,17 @@
+# Desired Command / Road Map
+
+# x = BoltzmannPDE()
+# x.init.time()
+# x.init.species()
+# x.init.position()
+# x.init.velocity()
+# x.init.print() prints all data of all grids
+#
+# x.setup()
+# x.setup.print() - prints grids as plot
+#
+# x.calc()
+#
+# x.animate()
+#
+# x.save_animation()
