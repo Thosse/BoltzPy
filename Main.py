854677f61ae8761a8077c22b1160a412f5a4aa13
Thosa <thomas.sasse@tu-ilmenau.de>
2018-03-29 15:46:05 +0200

    Slightly changed imports. giving the filename is unnecessary -> shortened from module.submodule.file to module.submodule

--- Main.py
+++ Main.py
@@ -1,24 +1,24 @@
 # Desired Command / Road Map
-import boltzmann.configuration.configuration as b_cnf
-import boltzmann.initialization.initialization as b_ini
-import boltzmann.calculation.calculation as b_cal
-import boltzmann.animation.animation as b_ani
+import boltzmann.configuration as b_cnf
+import boltzmann.initialization as b_ini
+import boltzmann.calculation as b_cal
+import boltzmann.animation as b_ani
 from datetime import datetime
 
 
 print('Starting Time:\n' + str(datetime.now()))
 cnf = b_cnf.Configuration()
-cnf.add_specimen(2)               # 2
-cnf.add_specimen(3)               # 3
-cnf.s._alpha *= 4                 # 2
-cnf.configure_time(10, 501, 10)      # 20 1001 10
+cnf.add_specimen(1)               # 2
+cnf.add_specimen(2)               # 3
+cnf.s._alpha *= 50                 # 2
+cnf.configure_time(10, 2510, 10)      # 20 1001 10
 cnf.configure_position_space(1,
-                             [200],       # 200
+                             [500],       # 200
                              step_size=0.1)   # 0.1
 cnf.configure_velocity_space(dimension=2,
-                             grid_points_x_axis=4,    # 8
+                             grid_points_x_axis=40,    # 8
                              max_v=1.5)       # 1.5
-cnf.collision_steps_per_time_step = 2     # 50
+cnf.collision_steps_per_time_step = 0     # 50
 cnf.print()
 
 ini = b_ini.Initialization(cnf)
@@ -27,17 +27,18 @@ ini.add_rule('Inner Point',
              [[0, 0], [0, 0], [0, 0]],
              [1, 1, 1],
              name='foo')
-ini.apply_rule(0, [0], [100])
+ini.apply_rule(0, [0], [250])
 ini.add_rule('Inner Point',
              [4, 2, 1],
              [[0, 0], [0, 0], [0, 0]],
              [1, 1, 1],
              name='bar')
-ini.apply_rule(1, [100], [200])
+ini.apply_rule(1, [250], [500])
 ini.print()
 
 cal = b_cal.Calculation(cnf, ini)
 cal.run()
 
 ani = b_ani.Animation(cnf)
+
 ani.run()
