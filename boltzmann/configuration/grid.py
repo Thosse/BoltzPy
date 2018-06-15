b22729e817a0858d7e39bd17ee0e25467e1cb625
Thosa <thomas.sasse@tu-ilmenau.de>
2018-06-15 15:49:52 +0200

    Moved some Grid properties into attributes. Moved Grid.boundaries property itno method, to indicate that it is calculated, not read.

--- boltzmann/configuration/grid.py
+++ boltzmann/configuration/grid.py
@@ -8,111 +8,56 @@ import numpy as np
 # Todo add check parameters
 # Todo break line in multi attribute docstring
 class Grid:
-    """Basic class for Positional-Space or Time-Space Grids."""
-    def __init__(self):
-        self._dim = 0
-        self._form = ''
-        self._multi = 1
-        self._d = 0.0
-        self._n = np.zeros(shape=(self.dim,), dtype=int)
-        self._size = 0
-        self._G = np.zeros(shape=(self.size, self.dim), dtype=int)
-        self._flag_is_centered = False
+    """Basic class for Positional-Space or Time-Space Grids.
 
-    @property
-    def dim(self):
-        """:obj:`int`: :obj:`Grid` dimensionality."""
-        return self._dim
+    Notes
+    -----
+    Note that changing :attr:`Grid.multi`
+    does not change the physical step size
+    or physical values of the :obj:`Grid`,
+    but allows features like adaptive Positional-Grids, or.
 
-    @property
-    def form(self):
-        """:obj:`str`:
-        Geometric form of :class:`Grid`,
-        must be an element of :attr:`~boltzmann.constants.SUPP_GRID_FORMS`.
-        """
-        return self._form
-
-    @property
-    def multi(self, ):
-        """:obj:`int`:
-        Determines the ratio of
-        physical step size / internal step size (:attr:`d`).
-
-        See :meth:`double_multiplicator`, :meth:`halve_multiplicator`.
-        :attr:`multi` does not change the physical values,
-        but enables a variety of features
-        (see :attr:`Configuration.t`, :attr:`Configuration.sv`)
-        """
-        return self._multi
-
-    @property
-    def d(self):
-        """:obj:`float` :
-        Internal step size of the grid.
-
-        Physical step size of uniform :class:`Grid` may differ
-        (see :attr:`multi`, :class:`Configuration`).
-        """
-        return self._d
-
-    @property
-    def n(self):
-        """:obj:`~numpy.ndarray` of :obj:`int` :
-        Number of grid points per dimension.
+    Attributes
+    ----------
+    form : :obj:`str`
+        Geometric form of the :class:`Grid`.
+        Must be an element of
+        :const:`~boltzmann.constants.SUPP_GRID_FORMS`.
+    dim : :obj:`int`
+        The :obj:`Grid` dimensionality. Must be in [1, 2, 3].
+    n : :obj:`~numpy.ndarray` of :obj:`int`
+        Number of :obj:`Grid` points per dimension.
         Array of shape (:attr:`dim`,).
-        """
-        return self._n
+    size : :obj:`int`
+        Total number of :obj:`Grid` points.
+    d : :obj:`float`
+        Smallest possible step size of the :obj:`Grid`.
+        Note that the physical step size of a uniform :obj:`Grid`
+        is :attr:`multi` * :attr:`d`.
+    multi : :obj:`int`
+        Ratio of physical step size / :attr:`d`.
+        Thus all values in :attr:`G` are multiples of :attr:`multi`.
+    G : :obj:`~np.ndarray` of :obj:`int`
+        G[i] denotes the physical value/coordinates of
+        :class:`Grid` point i
+        in multiples of :attr:`d`.
 
-    @property
-    def size(self):
-        """:obj:`int` :
-        Total number of grid points.
-        """
-        return self._size
-
-    @property
-    def G(self,):
-        """:obj:`~np.ndarray` of :obj:`int` :
-        Physical values of the :class:`Grid` points,
-        given in multiples of :attr:`d`.
-
-        G[i] denotes the physical value/coordinates of grid point i.
         Array of shape (:attr:`size`, :attr:`dim`)
-        """
-        return self._G
-
-    @property
-    def is_centered(self, ):
-        """:obj:`bool` :
-        True if Grid has been centered
-        (i.e. :meth:`center` was called).
+    is_centered : :obj:`bool`
+        True if :obj:`Grid` / :attr:`G` has been centered,
+        i.e. :meth:`center` was called.
         False otherwise.
-        """
-        return self._flag_is_centered
 
-    @property
-    def supported_forms(self):
-        """:obj:`set` of :obj:`str` :
-       Set of all currently supported geometric forms(:attr:`form`)
-       for the :class:`Grid`.
-       """
-        supported_forms = {'rectangular'}
-        return supported_forms
-
-    @property
-    def boundaries(self):
-        """:obj:`~numpy.ndarray` of :obj:`float` :
-        Minimum and maximum physical values over all :class:`Grid` points.
-
-        Array of shape (2, :attr:`dim`).
-        """
-        # in uninitialized Grids: Min/Max operation raises Errors
-        if self.size == 0:
-            return np.zeros((2, self.dim), dtype=float)
-        min_val = np.min(self.G, axis=0)
-        max_val = np.max(self.G, axis=0)
-        bound = np.array([min_val, max_val]) * self.d
-        return bound
+    """
+    def __init__(self):
+        self.form = ''
+        self.dim = 0
+        self.n = np.zeros(shape=(self.dim,), dtype=int)
+        self.size = 0
+        self.d = 0.0
+        self.multi = 1
+        self.G = np.zeros(shape=(self.size, self.dim), dtype=int)
+        self.is_centered = False
 
     #####################################
     #           Configuration           #
@@ -134,18 +79,18 @@ class Grid:
         form : :obj:`str`, optional
         multi : :obj:`int`, optional
         """
-        self._dim = dimension
-        self._n = np.array(number_of_points_per_dimension,
-                           dtype=int)
-        self._multi = multi
-        self._d = float(step_size) / self._multi
+        self.dim = dimension
+        self.n = np.array(number_of_points_per_dimension,
+                          dtype=int)
+        self.multi = multi
+        self.d = float(step_size) / self.multi
 
         if form == 'rectangular':
-            self._size = int(self.n.prod())
+            self.size = int(self.n.prod())
         else:
             message = "Unsupported Grid Form: {}".format(form)
             raise ValueError(message)
-        self._form = form
+        self.form = form
         self._construct_grid()
         self.check_integrity()
         return
@@ -166,7 +111,7 @@ class Grid:
         assert self.form == 'rectangular'
         grid_shape = (self.size, self.dim)
         # Create list of 1D grids for each dimension
-        list_of_1D_grids = [np.arange(0, self.n[i_d]*self._multi, self._multi)
+        list_of_1D_grids = [np.arange(0, self.n[i_d]*self.multi, self.multi)
                             for i_d in range(self.dim)]
         # Create mesh grid from 1D grids
         # Note that *[a,b,c] == a,b,c
@@ -184,7 +129,7 @@ class Grid:
                       "{}".format(self.dim)
             raise AttributeError(message)
         assert grid.shape == tuple(self.n) + (self.dim,)
-        self._G = grid.reshape(grid_shape)
+        self.G = grid.reshape(grid_shape)
         return
 
     def double_multiplicator(self):
@@ -192,9 +137,9 @@ class Grid:
 
         Also doubles all Entries in :attr:`G` and halves :attr:`d`.
         """
-        self._G *= 2
-        self._d /= 2
-        self._multi *= 2
+        self.G *= 2
+        self.d /= 2
+        self.multi *= 2
         return
 
     def halve_multiplicator(self):
@@ -206,9 +151,9 @@ class Grid:
                                     "should be multiples of 2."
         assert all(self.G % 2 == 0), "All Entries in :attr:`G`" \
                                      "should be multiples of 2."
-        self._G /= 2
-        self._d *= 2
-        self._multi /= 2
+        self.G /= 2
+        self.d *= 2
+        self.multi /= 2
         return
 
     def center(self):
@@ -220,8 +165,8 @@ class Grid:
             return
         alternation = self.multi*(self.n - 1)
         self.double_multiplicator()
-        self._G -= alternation
-        self._flag_is_centered = True
+        self.G -= alternation
+        self.is_centered = True
         return
 
     def revert_center(self):
@@ -235,15 +180,36 @@ class Grid:
                                     'The current multiplicator is {}' \
                                     ''. format(self.multi)
         alternation = (self.multi // 2) * (self.n - 1)
-        self._G += alternation
-        self._flag_is_centered = False
+        self.G += alternation
+        self.is_centered = False
         self.halve_multiplicator()
         return
 
     def reshape(self, shape):
         """Changes the shape of :attr:`G`."""
-        self._G = self._G.reshape(shape)
+        self.G = self.G.reshape(shape)
         return
+
+    #####################################
+    #           Utilities               #
+    #####################################
+    def boundaries(self):
+        """Returns Minimum and maximum physical values
+        over all :class:`Grid` points
+        in array of shape (2, :attr:`dim`).
+
+        Returns
+        -------
+        :obj:`~numpy.ndarray` of :obj:`float`
+
+        """
+        # in uninitialized Grids: Min/Max operation raises Errors
+        if self.size == 0:
+            return np.zeros((2, self.dim), dtype=float)
+        min_val = np.min(self.G, axis=0)
+        max_val = np.max(self.G, axis=0)
+        bound = np.array([min_val, max_val]) * self.d
+        return bound
 
     #####################################
     #           Serialization           #
@@ -317,10 +283,10 @@ class Grid:
         if self.G.ndim == 1:
             assert self.dim == 1
             assert self.G.shape == (self.size,)
-            assert self.boundaries.shape == (2,)
+            assert self.boundaries().shape == (2,)
         else:
             assert self.G.shape == (self.size, self.dim)
-            assert self.boundaries.shape == (2, self.dim)
+            assert self.boundaries().shape == (2, self.dim)
         assert type(self.multi) is int
         assert self.multi >= 1
         assert (self.G % self.multi == 0).all
@@ -343,7 +309,7 @@ class Grid:
         description += "Physical Step Size = {}\n".format(self.d * self.multi)
         description += 'Is centered Grid = {}\n'.format(self.is_centered)
         description += "Boundaries:\n"
-        description += '\t' + self.boundaries.__str__().replace('\n', '\n\t')
+        description += '\t' + self.boundaries().__str__().replace('\n', '\n\t')
         if write_physical_grids:
             description += '\n'
             description += 'Physical Grid:\n\t'
