0b18f296f89b3951ba4a5838814d69287c65d15a
Thosa <thomas.sasse@tu-ilmenau.de>
2018-06-16 14:10:01 +0200

    Bugfix: Last commit changed method names for Main.py. Main.py now uses the new Syntax.

--- Main.py
+++ Main.py
@@ -6,22 +6,21 @@ import boltzmann.animation as b_ani
 from datetime import datetime
 
 
-print('Starting Time:\n' + str(datetime.now()))
+print('Starting Time:\n' + str(datetime.now()) + '\n')
 cnf = b_cnf.Configuration()
 cnf.add_specimen(mass=1)               # 2
 cnf.add_specimen(mass=2)               # 3
 cnf.s.collision_rate_matrix *= 50                 # 2
-cnf.configure_time(0.01, 10, 1)      # 20 1001 10
-cnf.configure_position_space(1,
-                             [20],       # 200
-                             step_size=0.1)   # 0.1
-cnf.configure_velocity_space(dimension=2,
-                             grid_points_x_axis=4,    # 8
-                             max_v=1.5)       # 1.5
+cnf.set_time_grid(0.1, 11, 10)      # 20 1001 10
+cnf.set_position_grid(1,
+                      [21],  # 200
+                      grid_spacing=0.1)   # 0.1
+cnf.set_velocity_grids(grid_dimension=2,
+                       grid_points_on_single_axis=4,  # 8
+                       maximum_velocity=1.5)       # 1.5
 cnf.collision_steps_per_time_step = 1     # 50
-cnf.print(False)
+print(cnf.__str__(write_physical_grids=True))
 cnf.save()
-
 
 ini = b_ini.Initialization(cnf)
 ini.add_rule('Inner Point',
@@ -35,8 +34,8 @@ ini.add_rule('Inner Point',
              [[0, 0], [0, 0], [0, 0]],
              [1, 1, 1],
              name='bar')
-ini.apply_rule(1, [10], [20])
-ini.print()
+ini.apply_rule(1, [10], [21])
+ini.print(True)
 
 cal = b_cal.Calculation(cnf, ini)
 cal.run()
