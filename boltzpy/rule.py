b4f1f4bbaf2bb47b13f85e69f1a857e89b0fac10
Thomas Sasse <thomas.sasse@tu-ilmenau.de>
2020-04-08 10:24:32 +0200

    Add checks for NAN values ini Rule.initial_state
    Also add a proper message for initial state checks

--- boltzpy/rule.py
+++ boltzpy/rule.py
@@ -390,7 +390,13 @@ class Rule(bp.BaseClass):
         if initial_state is not None:
             assert isinstance(initial_state, np.ndarray)
             assert initial_state.dtype == float
-            assert np.all(initial_state >= 0)
+            assert not np.any(np.isnan(initial_state)), (
+                "Initial state contains NAN values!"
+            )
+            assert np.all(initial_state >= 0), (
+                "Minimal value = {}\n"
+                "Initial values:\n{}".format(np.min(initial_state), initial_state)
+            )
             # Todo test initial state, moments should be matching (up to 10^-x)
         return
 
