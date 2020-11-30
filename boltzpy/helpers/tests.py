77af3b4a3f32f22b5c9db8dd27960679807e47cd
Thomas Sasse <thomas.sasse@tu-ilmenau.de>
2020-11-30 14:48:22 +0100

    Edit test_helper.assert function
    function now compares groups instead of files.
    This allows recursions for sub groups.

--- boltzpy/helpers/tests.py
+++ boltzpy/helpers/tests.py
@@ -2,63 +2,39 @@ import h5py
 import numpy as np
 
 
-# Todo make this a set of strings
-def get_all_keys(hdf_object):
-    """:obj:`list` [ :obj:`str` ]:
-    The list of all groups in the hdf5 file.
-    """
-    keys = []
-    if isinstance(hdf_object, h5py.Dataset):
-        return keys
-    if isinstance(hdf_object, h5py.Group):
-        for (key, subgroup) in hdf_object.items():
-            subkeys = get_all_keys(subgroup)
-            keys.append(key)
-            keys = keys + ["{}/{}".format(key, subkey)
-                           for subkey in subkeys]
-    else:
-        raise Exception
-    return keys
-
-
-def assert_files_are_equal(addresses):
-    assert len(addresses) >= 2, (
+def assert_hdf_groups_are_equal(*hdf5_groups):
+    assert len(hdf5_groups) >= 2, (
         "At least 2 files must be given")
-    n_files = len(addresses)
+    n_groups = len(hdf5_groups)
+    for idx in range(n_groups - 1):
+        group_1 = hdf5_groups[idx]
+        keys_1 = group_1.keys()
+        group_2 = hdf5_groups[idx + 1]
+        keys_2 = group_2.keys()
+        # Keys must be equal!
+        assert keys_1 == keys_2
+        # Todo compare attributes as well?
 
-    for idx in range(n_files - 1):
-        with h5py.File(addresses[idx], mode='r') as file_1:
-            keys_1 = get_all_keys(file_1)
-            with h5py.File(addresses[idx + 1], mode='r') as file_2:
-                keys_2 = get_all_keys(file_2)
-                assert keys_1 == keys_2, (
-                    "The Files:\n\t{}\n\t{}"
-                    "\ncontain different Groups or Data sets"
-                    "".format(addresses[idx], addresses[idx + 1]))
-
-            # All Datasets must have equal values
-                for key in keys_1:
-                    # Both must have the same hdf type (Group or Dataset)
-                    assert isinstance(file_1[key], type(file_2[key])), (
-                            "Differing data types for {}:\t"
-                            "{} vs {}"
-                            "".format(key, type(file_1[key]), type(file_2[key])))
-                    # skip groups
-                    if not isinstance(file_1[key], h5py.Dataset):
-                        continue
-                    val_1 = file_1[key][()]
-                    val_2 = file_2[key][()]
-                    assert isinstance(val_1, type(val_2)), (
-                            "Differing data types for {}:\t"
-                            "{} vs {}".format(key, type(val_1), type(val_2)))
-                    if isinstance(val_1, np.ndarray):
-                        if val_1.dtype == float:
-                            assert np.allclose(val_1, val_2), (
-                                "Differing values for {}".format(key))
-                        else:
-                            assert np.array_equal(val_1, val_2), (
-                                "Differing values for {}".format(key))
+        for key in keys_1:
+            value_1 = group_1[key]
+            value_2 = group_1[key]
+            # Both must have the same hdf type (Group or Dataset)
+            assert isinstance(value_1, type(value_2))
+            # test groups recursively
+            if isinstance(value_1, h5py.Group):
+                assert_hdf_groups_are_equal(value_1, value_2)
+            # Datasets must be equal
+            elif isinstance(value_1, h5py.Dataset):
+                value_1 = value_1[()]
+                value_2 = value_2[()]
+                assert isinstance(value_1, type(value_2))
+                if isinstance(value_1, np.ndarray):
+                    assert value_1.shape == value_2.shape
+                    assert value_1.dtype == value_2.dtype
+                    if value_1.dtype == float:
+                        assert np.allclose(value_1, value_2)
                     else:
-                        assert val_1 == val_2, (
-                            "Differing values for {}".format(key))
+                        assert np.array_equal(value_1, value_2)
+                else:
+                    assert value_1 == value_2
     return
--- boltzpy/test_simulation.py
+++ boltzpy/test_simulation.py
@@ -14,32 +14,22 @@ from boltzpy.test_BaseClass import DIRECTORY, TMP_FILE
 ###################################
 #           Setup Cases           #
 ###################################
+def file_path(name):
+    assert isinstance(name, str)
+    return DIRECTORY + "Simulation/" + name + ".hdf5"
 
-FILE = DIRECTORY + 'Simulations.hdf5'
-_open_file = h5py.File(FILE, mode="r")
-SIMULATIONS = dict()
-SIMULATIONS["2D_small/Simulation"] = bp.Simulation(
-    timing=GRIDS["Grid/2D_small"],
-    geometry=GEOMETRIES["Geometry/2D_small"],
-    model=MODELS["Model/2D_small"],
-    file=_open_file["2D_small/Simulation"],
-    log_state=True)
-SIMULATIONS["equalMass/Simulation"] = bp.Simulation(
-    timing=GRIDS["Grid/equalMass"],
-    geometry=GEOMETRIES["Geometry/equalMass"],
-    model=MODELS["Model/equalMass"],
-    file=_open_file["equalMass/Simulation"],
-    log_state=True)
 
 SIM_PARAMS = [
-    ("2D_small/Simulation", {"timing": GRIDS["Grid/2D_small"],
-                             "geometry": GEOMETRIES["Geometry/2D_small"],
-                             "model": MODELS["Model/2D_small"],
-                             "log_state": True}),
-    ("equalMass/Simulation", {"timing": GRIDS["Grid/equalMass"],
-                              "geometry": GEOMETRIES["Geometry/equalMass"],
-                              "model": MODELS["Model/equalMass"],
-                              "log_state": True})]
+    (file_path("2D_small"),
+     {"timing": GRIDS["Grid/2D_small"],
+      "geometry": GEOMETRIES["Geometry/2D_small"],
+      "model": MODELS["Model/2D_small"],
+      "log_state": True}),
+    (file_path("equalMass"),
+     {"timing": GRIDS["Grid/equalMass"],
+      "geometry": GEOMETRIES["Geometry/equalMass"],
+      "model": MODELS["Model/equalMass"],
+      "log_state": True})]
 
 SAVED_MOMENTS = [
     "number_density",
@@ -51,43 +41,35 @@ SAVED_MOMENTS = [
     "energy_flow"]
 
 
-# Todo splitte file in separate dateien, pro simulation eins
-def setup_file(file_address=FILE):
-    if file_address == FILE:
-        reply = input("You are about to reset the simulations test file. "
-                      "Are you Sure? (yes, no)\n")
-        if reply != "yes":
-            print("ABORTED")
-            return
-        else:
-            _open_file.close()
+def setup_files():
+    reply = input("You are about to reset the simulations test files. "
+                  "Are you Sure? (yes, no)\n")
+    if reply != "yes":
+        print("ABORTED")
+        return
 
-    with h5py.File(file_address, mode="w") as file:
-        for (key, item) in SIMULATIONS.items():
-            assert isinstance(item, bp.Simulation)
-            file.create_group(key)
-            item.compute(file[key])
-            item.file = file[key]
-    return
-
-
-def test_setup_creates_same_file():
-    setup_file(TMP_FILE)
-    test_helper.assert_files_are_equal([FILE, TMP_FILE])
+    for (path, params) in SIM_PARAMS:
+        file = h5py.File(path, mode="w")
+        sim = bp.Simulation(**params)
+        assert isinstance(sim, bp.Simulation)
+        sim.compute(file)
+    print("Sucessfully set up all Test files")
+    input("Press any key to continue!")
     return
 
 
 @pytest.fixture(params=SIM_PARAMS, scope='class')
 def simulation(request):
-    (key, params) = request.param
-    with h5py.File(TMP_FILE, mode="w") as new_file:
-        with h5py.File(FILE, mode="r") as old_file:
+    (path, params) = request.param
+    assert os.path.exists(path)
+    with h5py.File(path, mode="r") as old_file:
+        with h5py.File(TMP_FILE, mode="w") as new_file:
             sim = bp.Simulation(file=new_file, **params)
             sim.compute(hdf_group=new_file)
             # inject class variables
             request.cls.sim = sim
             request.cls.new_file = new_file
-            request.cls.old_file = old_file[key]
+            request.cls.old_file = old_file
             # pass request.cls as self to the test class
             yield
     # teardown
@@ -96,22 +78,17 @@ def simulation(request):
 
 @pytest.mark.usefixtures("simulation")
 class TestSimulation:
-    def test_old_file_exists(self):
-        assert os.path.exists(FILE), (
-            "The test file {} is missing.".format(FILE))
-
-    # def test_hdf5_groups_exist(self):
-    #     with h5py.File(FILE, mode="r") as file:
-    #         assert key in file.keys(), (
-    #             "The group {} is missing in the test file-".format(key))
+    def test_new_file_equals_old_file(self):
+        test_helper.assert_hdf_groups_are_equal(self.new_file.file,
+                                                self.old_file.file)
+        return
 
     def test_load_from_file(self):
-        with h5py.File(FILE, mode="r") as file:
-            old_sim = bp.Simulation.load(self.old_file)
-            new_sim = self.sim
-            assert isinstance(old_sim, bp.Simulation)
-            assert isinstance(new_sim, bp.Simulation)
-            assert old_sim == new_sim
+        old_sim = bp.Simulation.load(self.new_file)
+        new_sim = self.sim
+        assert isinstance(old_sim, bp.Simulation)
+        assert isinstance(new_sim, bp.Simulation)
+        assert old_sim == new_sim
 
     def test_computed_same_output_moments(self):
         old_keys = self.old_file["results"].keys()
