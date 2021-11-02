d54ccbdf88094a0a3e3bdf3328a80a0c3d16d190
Thomas Sasse <thomas.sasse@tu-ilmenau.de>
2021-11-02 05:34:15 +0100

    Improve figure on translated velocitites

--- bin/plt_translation_of_collisions.py
+++ bin/plt_translation_of_collisions.py
@@ -1,8 +1,15 @@
 import boltzpy as bp
 import numpy as np
+import matplotlib
+matplotlib.use('Qt5Agg')
+matplotlib.rcParams['text.usetex'] = True
+matplotlib.rcParams['text.latex.preamble'] = r'\usepackage{amsmath, amssymb, faktor}'
 import matplotlib.pyplot as plt
+from fonts import fs_title, fs_legend, fs_label, fs_suptitle, fs_ticks
 
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, constrained_layout=True, sharey="all", sharex="all")
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3,
+                                    figsize=(12.75, 5.),
+                                    sharey="all", sharex="all")
 
 model_1 = bp.CollisionModel([2],
                             [(4, 4)],
@@ -31,9 +38,10 @@ cols_bad = [base_mix + c for c in [[0,  2], [2,  2], [4,  2], [2, 4],
 for c in cols_bad:
     ax2.plot(c[:, 0], c[:, 1],
              color="red",
-             linewidth=1,
-             linestyle="dashed",
-             dashes=(20, 15))
+             linewidth=2,
+             linestyle="dotted",
+             # dashes=(20, 15)
+             )
 for c in cols_mix:
     ax2.plot(c[:, 0], c[:, 1],
              color="darkgray",)
@@ -49,22 +57,36 @@ partitions = model_2.group(key_distance,
 
 for point, dist in zip(model_2.subgrids(0).iG, key_distance):
     ax3.annotate(s='', xy=point - 0.15*dist, xytext=point - 0.85*dist,
-                 arrowprops=dict(arrowstyle='<->', linewidth=0.5, color="gray"))
+                 arrowprops=dict(arrowstyle='<->', linewidth=2, color="gray"))
 
 for point in model_2.subgrids(1).iG:
     point = point - offset
     ax3.text(point[0], point[1], "1",
-             bbox={"boxstyle": "circle", "color": "orange"})
+             bbox={"boxstyle": "circle", "color": "orange"},
+             fontsize=13)
 
 for p, prt in enumerate(partitions):
     for point in prt:
         point = point - offset
         ax3.text(point[0], point[1], str(p + 1),
-                 bbox={"boxstyle": "circle", "color": "lightsteelblue"})
+                 bbox={"boxstyle": "circle", "color": "lightsteelblue"},
+                 fontsize=13)
 #
 #     ax3.scatter(*p.transpose(), marker='o', alpha=0.5, s=50, c=colors[i])
 ax3.set_aspect('equal')
+for ax in [ax1, ax2, ax3]:
+    ax.set_xticks([])
+    ax.set_yticks([])
+ax1.set_title(r"Shifted Collisions in $\mathfrak{C}^{s,s}$",
+              fontsize=fs_title)
+ax2.set_title(r"Shifting Collisions in a Mixture",
+              fontsize=fs_title)
+ax3.set_title(# r"Equivalence Classes of "
+              r"$\faktor{\mathfrak{V}^1}{dist_2}$"
+              r" and $\faktor{\mathfrak{V}^2}{dist_1}$",
+              fontsize=fs_title)
 
+plt.subplots_adjust(top=0.7)
 plt.tight_layout()
-plt.savefig(bp.SIMULATION_DIR + "/plt_translation_of_collisions.eps")
-plt.show()
+plt.savefig(bp.SIMULATION_DIR + "/plt_translation_of_collisions.pdf")
+# plt.show()
